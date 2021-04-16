948d90559dd33b30eaef64153b1f38d0d2883d98
Victor Granados <vgranados_cr_90@hotmail.com>
2021-04-16 10:14:08 -0600

    Se agrega la fecha y se cambia la logica para mostrar los datos en el for

--- script/noticias.js
+++ script/noticias.js
@@ -1,16 +1,44 @@
 'use strict'
 
+//Así se llena un JSON y se recorre
+
+/*var mynews = []
+var indice
+
+mynews.push({
+            "headline"        : docs.response.docs[i].headline.main,
+            "abstract"        : docs.response.docs[i].abstract,
+            "byline_original" : docs.response.docs[i].byline.original,
+            "imagen"          : multimedia + docs.response.docs[i].multimedia[0].url,
+            "paragraph"       : docs.response.docs[i].lead_paragraph
+})
+
+for(indice in mynews){
+    var headline = document.createElement('h1')
+    var imagen = document.createElement('img')
+    var paragraph = document.createElement('p')
+                
+    headline.append(mynews[indice].headline)
+    imagen.src = mynews[indice].imagen
+    paragraph.append(mynews[indice].paragraph)
+      
+    head.append(headline)
+    img.append(imagen)
+    head.append(paragraph)
+}*/
+
 //Así se obtiene el API en jQuery
 /*$.get("https://api.nytimes.com/svc/archive/v1/2020/12.json?api-key=n7YKG0MPOmRl8ESF86N0V6VXLSUubtdo", function(data){
     console.log(data)
 })*/
 
+var fecha = new Date()
 var consulta = document.getElementById('consulta')
+var date = document.getElementById('date')
 var head = document.getElementById('head')
 var img = document.getElementById('img')
 
-var mynews = []
-var indice
+date.innerHTML = "Today is: " + fecha.getDate() + " / " + fecha.getMonth() + " / " + fecha.getFullYear()
 
 consulta.addEventListener('click', function(){
 
@@ -30,27 +58,19 @@ consulta.addEventListener('click', function(){
             //console.log(docs.response.docs[3])
             
             for(var i = 0; i <= 5; i ++){
-                mynews.push({
-                                "headline"        : docs.response.docs[i].headline.main,
-                                "abstract"        : docs.response.docs[i].abstract,
-                                "byline_original" : docs.response.docs[i].byline.original,
-                                "imagen"          : multimedia + docs.response.docs[i].multimedia[0].url,
-                                "paragraph"       : docs.response.docs[i].lead_paragraph
-                })
-            }
-            
-            for(indice in mynews){
+
                 var headline = document.createElement('h1')
                 var imagen = document.createElement('img')
                 var paragraph = document.createElement('p')
-                
-                headline.append(mynews[indice].headline)
-                imagen.src = mynews[indice].imagen
-                paragraph.append(mynews[indice].paragraph)
-      
+
+                headline.append(docs.response.docs[i].headline.main)
+                imagen.src = multimedia + docs.response.docs[i].multimedia[0].url
+                paragraph.append(docs.response.docs[i].lead_paragraph)
+
                 head.append(headline)
                 img.append(imagen)
                 head.append(paragraph)
+
             }
         })
     
